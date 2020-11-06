d905477bebe5bfe75e6619efdd70caf93e7d64fb
Vinny Martins <41807230+martins-vds@users.noreply.github.com>
2020-11-06 08:43:59 -0700

    Update fetchSaveRepo.js

--- fetchSaveRepo.js
+++ fetchSaveRepo.js
@@ -2,13 +2,13 @@ const fetch = require('node-fetch');
 const fs = require('fs');
 
 const path = 'repos.txt';
-const repoUrl = 'https://api.github.com/users/peripona/repos?per_page=200';
+const repoUrl = 'https://api.github.com/users/martins-vds/repos?per_page=200';
 
 const logger = fs.createWriteStream(path, {
   flags: 'a',
 });
 
-const fetchRepos = (url = 'https://api.github.com/users/peripona/repos?per_page=200') => {
+const fetchRepos = (url = 'https://api.github.com/users/martins-vds/repos?per_page=200') => {
   fetch(url)
     .then(reply => reply.json())
     .then((results) => {
